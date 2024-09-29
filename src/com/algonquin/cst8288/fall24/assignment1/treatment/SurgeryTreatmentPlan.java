432160b47372915bbe6e9cdb5340b6c5cc43ea09
Peter Stainforth <stai0029@algonquinlive.com>
2024-09-29 14:08:04 -0400

    Implemented SurgeryTreatmentPlan 1st draft.

--- src/com/algonquin/cst8288/fall24/assignment1/treatment/SurgeryTreatmentPlan.java
+++ src/com/algonquin/cst8288/fall24/assignment1/treatment/SurgeryTreatmentPlan.java
@@ -1,6 +1,7 @@
 package com.algonquin.cst8288.fall24.assignment1.treatment;
 
 import com.algonquin.cst8288.fall24.assignment1.patient.Patient;
+import com.alognquin.cst8288.fall24.assignment1.Constants;
 
 /**
  * 
@@ -19,11 +20,60 @@ import com.algonquin.cst8288.fall24.assignment1.patient.Patient;
 
 
 public class SurgeryTreatmentPlan implements TreatmentPlan{
-
+	private String diagnosis;
+	private String riskFactor;
+	private String surgery;
+	private String fasting;
+	private String followup;
+	
+	public SurgeryTreatmentPlan(String diagnosis, String surgery) {
+		this.diagnosis = diagnosis;
+	}
+	
+	public void setDiagnosis(String diagnosis) {
+		this.diagnosis = diagnosis;
+	}
+	
+	public void setSurgery(String surgery) {
+		this.surgery = surgery;
+	}
 	@Override
 	public String createTreatmentPlan(Patient patient) {
 		// TODO Auto-generated method stub
-		return null;
+		if(patient.getLifeStage() == null) {
+			throw new IllegalStateException("Life stage must not be null");
+		}
+		switch(patient.getLifeStage()) {
+		case Constants.CHILD:
+			riskFactor = Constants.HIGH;
+			fasting = "No";
+			followup = "next day";
+			break;
+		case Constants.YOUTH:
+			riskFactor = Constants.MEDIUM;
+			fasting = "2 hours";
+			followup = "in one week";
+			break;
+		case Constants.ADULT:
+			riskFactor = Constants.LOW;
+			fasting = "8 hours";
+			followup = "in one month";
+			break;
+		}
+		String blurb = String.format(
+				"The patient is diagnosed with %s and needs surgery.%n"
+				+ "The patient is %d years old with a %s risk factor.%n"
+				+ "%s surgery is required.%n"
+				+ "%s fasting is required.%n"
+				+ "A followup appointment will be %s.%n",
+				diagnosis,
+				patient.getAge(),
+				riskFactor,
+				surgery,
+				fasting,
+				followup
+				);
+		return blurb;
 	}
 
 	
